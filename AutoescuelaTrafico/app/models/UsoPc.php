8f41d7f17a1b317fa157b26ced0df8ec651633a9
marespcue1 <57679059+marespcue1@users.noreply.github.com>
2020-04-12 21:47:54 +0200

    Clase Ordenador y UsoPc

---
+++ AutoescuelaTrafico/app/models/UsoPc.php
@@ -0,0 +1,68 @@
+<?php
+class UsoPc extends EntidadBase {
+	private $id;
+	private $fecha;
+	private $horaComienzo;
+	private $horaFin;
+
+	public function __construct($adapter) {
+		$table = "UsoPc";
+		parent::__construct($table, $adapter);
+	}
+
+	/**
+     * Get fecha
+     *
+     * @return VariableType
+     */
+    public function getFecha(){
+        return $this->fecha;
+    }
+
+	/**
+     * Set fecha
+     *
+     * @param VariableType $fecha
+     */
+    public function setFecha($fecha){
+        $this->fecha = $fecha;
+    }
+
+	/**
+     * Get horaComienzo
+     *
+     * @return VariableType
+     */
+    public function getHoraComienzo(){
+        return $this->horaComienzo;
+    }
+
+	/**
+     * Set horaComienzo
+     *
+     * @param VariableType $horaComienzo
+     */
+    public function setHoraComienzo($horaComienzo){
+        $this->horaComienzo = $horaComienzo;
+    }
+
+	/**
+     * Get horaFin
+     *
+     * @return VariableType
+     */
+    public function getHoraFin(){
+        return $this->horaFin;
+    }
+
+	/**
+     * Set horaFin
+     *
+     * @param VariableType $horaFin
+     */
+    public function setHoraFin($horaFin){
+        $this->horaFin = $horaFin;
+    }
+}
+
+?>
