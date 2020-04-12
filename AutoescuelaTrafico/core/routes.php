3b2f4359861bad6691e2ff4c9b72c1cdcca00dd1
alex-sant17 <63409808+alex-sant17@users.noreply.github.com>
2020-04-12 19:51:09 +0200

    Enrutador
    archivo routes.php

---
+++ AutoescuelaTrafico/core/routes.php
@@ -0,0 +1,29 @@
+<?php
+//FUNCIONES PARA EL FRONT CONTROLLER (index.php)
+
+function loadController($controller){
+	$controller = ucwords($controller).'Controller';
+	$strFileController='controller/'.$controller.'.php';
+
+	if(!is_file($strFileController)){
+		$strFileController='controllers/'.ucwords(DEFAULT_CONTROLLER).'Controller.php';
+	}
+
+	require_once $strFileController;
+	$controllerObj = new $controller();
+	return $controllerObj;
+}
+function loadAction($controllerObj, $action){
+	$action = $action;
+	$controllerObj->$action();
+}
+
+function doAction($controllerObj){
+	if(isset($_GET["action"]) && method_exists($controllerObj, $_GET["action"])){
+		loadAction($controllerObj, $_GET["action"]);
+	}
+	else{
+		loadAction($controllerObj, DEFAULT_ACTION);
+	}
+}
+?>
