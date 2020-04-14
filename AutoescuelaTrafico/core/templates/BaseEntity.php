dbbbdb3968d6cc50d816a32e60f10917381003a0
alex-sant17 <63409808+alex-sant17@users.noreply.github.com>
2020-04-14 22:03:21 +0200

    Core update

--- AutoescuelaTrafico/core/templates/BaseEntity.php
+++ AutoescuelaTrafico/core/templates/BaseEntity.php
@@ -1,40 +1,5 @@
 <?php
 class BaseEntity{
-    private $table;
-    private $db;
- 
-    public function __construct($table, $adapter) {
-        $this->table=(string) $table;
-        $this->db = $adapter;
-    }
-     
-    public function db(){
-        return $this->db;
-    }
-     
-    public function getAll(){
-        $query=$this->db->query("SELECT * FROM $this->table");
- 
-        while ($row = $query->fetchObject()) {
-           $resultSet[]=$row;
-        }
-         
-        return $resultSet;
-    }
-     
-    public function getBy($column,$value){
-        $query=$this->db->query("SELECT * FROM $this->table WHERE $column='$value'");
- 
-        while($row = $query->fetchObject()) {
-           $resultSet[]=$row;
-        }
-         
-        return $resultSet;
-    }
-    
-    public function deleteBy($column,$value){
-        $query=$this->db->query("DELETE FROM $this->table WHERE $column='$value'");
-        return $query;
-    }
-}
+    //Entidad base, metodos genericos como __tostring y parse
+}     
 ?>
