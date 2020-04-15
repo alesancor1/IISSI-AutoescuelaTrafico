fa513835de4dd2e0ac0932ad7ddc93d9a738daa9
marespcue1 <57679059+marespcue1@users.noreply.github.com>
2020-04-15 17:39:55 +0200

    a

---
+++ AutoescuelaTrafico/core/entities/Examen.php
@@ -0,0 +1,49 @@
+<?php
+class Examen extends Calificacion{
+	
+	protected $fecha;
+	protected $calificacion;
+	
+	public function __construct($child){
+		parent::__construct($child);
+	}
+	
+
+	/**
+     * Get fecha
+     *
+     * @return VariableType
+     */
+    public function getFecha(){
+        return $this->fecha;
+    }
+
+	/**
+     * Set fecha
+     *
+     * @param VariableType $fecha
+     */
+    public function setFecha($fecha){
+        $this->fecha = $fecha;
+    }
+
+	/**
+     * Get calificacion
+     *
+     * @return VariableType
+     */
+    public function getCalificacion(){
+        return $this->calificacion;
+    }
+
+	/**
+     * Set calificacion
+     *
+     * @param VariableType $calificacion
+     */
+    public function setCalificacion($calificacion){
+        $this->calificacion = $calificacion;
+    }
+}
+
+?>
