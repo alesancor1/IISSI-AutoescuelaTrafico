c9eb7888f9410413271faf93a8e3638e4d1e9d70
TGB07 <63409689+TGB07@users.noreply.github.com>
2020-04-15 21:47:36 +0200

    Error actualizado a HTML + imagen error

--- AutoescuelaTrafico/app/views/error.php
+++ AutoescuelaTrafico/app/views/error.php
@@ -1,15 +1,50 @@
-<?php
-echo "Ha sucedido un error :(";
-echo "<div>";
-switch($tipo){
-	case "conexionDB": echo "ha ocurrido un error al conectarse al servidor de base de datos."; break;
-	case "notLoggedIn": echo "debes iniciar sesion para acceder a estas funciones."; break;
-	case "wrongLogin": echo "No tienes acceso a esta funcion";break;
-	default: echo "error desconocido";
-}
-echo "</div>";
+<!DOCTYPE html>
+<html>
 
-$url = "location.href='./index.php';";
-echo '<div><input type="button" onclick='.$url. ' value="Volver al inicio"/></div>';
-exit();
-?>
+<body>
+
+<div class='errorTOP'>
+	
+	<h1>HA SUCEDIDO UN ERROR :(</h1>
+	<img class="errorImagen" src="./img/errorCrash.png" alt="Error al cargar la imagen. Hoy no es tu día ;("/>
+
+<?php
+switch($tipo){
+	case "conexionDB": ?>
+		<div class="errorConexionDB">
+			<p>Ha ocurrido un error al conectarse al servidor de base de datos.</p>
+		</div> 
+		<?php ; break;
+		
+	case "notLoggedIn": ?>
+		<div class="errorNotLoggedIn">
+			<p>Debes iniciar sesion para acceder a estas funciones.</p>
+		</div>
+	<?php ; break;
+		
+	case "wrongLogin": ?>
+		<div class="errorWrongLogin">
+			<p>No tienes acceso a esta funcion.</p>
+		</div>
+		<?php ; break;
+	
+	default:?>
+		<div class="errorWrongLogin">
+			<p>Error desconocido.</p>
+		</div>
+		<?php ;
+} ?>
+
+<?php
+	$url = "location.href='./index.php';"; ?>
+	<div class="botonVolverAtrásError">
+	<?php echo '<input type="button" onclick='.$url. ' value="Volver al inicio"/>' ?>
+	</div>
+
+	<?php
+	exit();
+	?>
+</div>
+
+</body>
+</html>
