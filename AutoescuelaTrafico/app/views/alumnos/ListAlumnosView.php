23dc0fa2966c8b838d538012c874fa85bf821281
TGB07 <63409689+TGB07@users.noreply.github.com>
2020-05-28 00:59:47 +0200

    #filtro lista de alumnos (vista profesor)

--- AutoescuelaTrafico/app/views/alumnos/ListAlumnosView.php
+++ AutoescuelaTrafico/app/views/alumnos/ListAlumnosView.php
@@ -10,6 +10,14 @@
 			
 			<h1>Lista de alumnos:</h1>
 			<br>
+		
+		<div class="filtros">
+			<form class="" id="" action="?controller=Alumnos&action=listaProfesor" method="POST">
+				<label for="filtro">Buscar por nombre, apellidos o DNI:</label>
+				<input type="text" id="filtro" name="filtro">
+				<input type="submit" hidden>
+			</form>
+		</div>
 			
 		<table>
 			<?php 
