da159b51f1b75a13e7ec8d29d24c14aa9124ce20
TGB07 <63409689+TGB07@users.noreply.github.com>
2020-05-28 20:18:06 +0200

    #index + menubar miniupdate

--- AutoescuelaTrafico/public/index.php
+++ AutoescuelaTrafico/public/index.php
@@ -1,3 +1,7 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -5,6 +9,8 @@
     	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
        	<link rel="stylesheet" href="./css/paginatorButtons.css">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 	</head>
 </html>
 
