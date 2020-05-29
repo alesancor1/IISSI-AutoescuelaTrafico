15f6a2ec77bf808dd18a041651a15a7fe02328d8
alex-sant17 <63409808+alex-sant17@users.noreply.github.com>
2020-05-29 19:54:54 +0200

    nuevos script sql

--- AutoescuelaTrafico/app/models/AlumnosAdminModel.php
+++ AutoescuelaTrafico/app/models/AlumnosAdminModel.php
@@ -22,16 +22,25 @@ class AlumnosAdminModel extends BaseModel {
 	}
 	
 	public function insert($dni, $nombre, $apellidos, $fechaNacimiento, $permisoMatriculado, $oferta){
+		$apellidosArr = split(" ",$apellidos);
+		$username = substr($nombre, 0, 3) . substr($apellidosArr[0], 0, 3) . substr($apellidosArr[1], 0, 3);
+		$this -> ejecutaSql("INSERT INTO LOGIN (USUARIO, DNI, PASS, TIPO) VALUES ('$username', '$dni', 'traficoALUM', 'Alumno')");
+
 		$this->ejecutaSql("INSERT INTO ACCESOWEB(USUARIO,CLAVEPORDEFECTO,FECHARENOVACION,FECHACADUCIDAD,WEB) VALUES ('$dni',DBMS_RANDOM.STRING('x',12),sysdate,sysdate+61,1)");	
 		$this->ejecutaSql("INSERT INTO Alumnos (DNI, NOMBRE, APELLIDOS, FECHANACIMIENTO,ACCESOWEB) VALUES ('$dni', '$nombre', '$apellidos', to_date('$fechaNacimiento','YYYY-MM-DD'),'$dni')");
 		$this->ejecutaSql("INSERT INTO MATRICULAS (FECHA, PERMISOMATRICULADO, ALUMNO, OFERTA) VALUES (sysdate, '$permisoMatriculado', '$dni', '$oferta')");	
 	}
 	
 	public function delete($dni) {
-		$this->ejecutaSql("DELETE FROM ALUMNOS WHERE DNI='$dni'");
-		$this->ejecutaSql("DELETE FROM ACCESOWEB WHERE USUARIO='$dni'");
+		try{
+			$this->ejecutaSql("DELETE FROM LOGIN WHERE DNI = '$dni'");
+			$this->ejecutaSql("DELETE FROM ALUMNOS WHERE DNI='$dni'");
+			$this->ejecutaSql("DELETE FROM ACCESOWEB WHERE USUARIO='$dni'");
+		}
+		catch (Exception $e){
+			echo $e;
+		}
 	}
-
 
 	// CALIFICACIONES
 	public function getAlumnosSinPaginar() {
