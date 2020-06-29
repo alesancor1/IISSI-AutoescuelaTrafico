5967000c6449c415be7c7327a63348603c4c0067
alex-sant17 <63409808+alex-sant17@users.noreply.github.com>
2020-06-29 23:38:48 +0200

    View rendering class

---
+++ core/helpers/ViewHelpers.php
@@ -0,0 +1,10 @@
+<?php
+class ViewHelpers{
+     
+    public static function url($controlador=DEFAUL_CONTROLLER,$accion=DEFAULT_ACTION){
+        $urlString="index.php?controller=".$controlador."&action=".$accion;
+        return $urlString;
+    }
+
+}
+?>
